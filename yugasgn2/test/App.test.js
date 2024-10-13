806367745e26a81fff3590426b4aa28ff11aae64
YugBandhara <ybandhara@gmail.com>
2024-10-13 13:15:56 -0400

    Test Flow

--- yugasgn2/test/App.test.js
+++ yugasgn2/test/App.test.js
@@ -1,14 +1,8 @@
-// app.test.js
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../src/App';
 
 test('renders the app component', () => {
-  // Render the App component
   render(<App />);
-
-  // Check if the text "Hello, World!" is present in the document
   const headingElement = screen.getByText(/Yug-Workflow/i);
-  
-  // Assert that the heading is in the document
   expect(headingElement).toBeInTheDocument();
 });
